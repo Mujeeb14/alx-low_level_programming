2f1b539373b7100483bb9d53264bd888bb64ce36
Mujeeb14 <fasasimujeebopeyemi.fmo@gmail.comexitgit config --global user.name Fasasi>
2023-02-16 16:52:15 -0800

    printf commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,14 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * return: Always 0 (Succeed)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a place of art,\n");
+
+	return (0);
+}
